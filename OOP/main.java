9a185446db86524ecb77b288ee966d6415470715
Vladimír Hanhur <aqgz@email.cz>
2025-04-10 20:01:37 +0200

    update

---
+++ OOP/main.java
@@ -0,0 +1,15 @@
+public class main {
+    byte b = 0; // 1 byte(256) -128 - + 127
+    short s = 11234; // 2 byte(~65000) -32 - + 32
+    int i = 233456789; // 4 byte(4)
+    long  l = 2345678; // 8 byte
+    float f = 12.354f; // 4 byte
+    double d = 1234.23528; // 8 byte
+    boolean bool = true;
+    char c = 'g';
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
