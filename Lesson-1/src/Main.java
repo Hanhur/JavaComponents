21991c795452ed08abb62e3fee18d0705ee02b53
Vladimír Hanhur <aqgz@email.cz>
2025-04-19 18:35:47 +0200

    update

---
+++ Lesson-1/src/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 10;
+        int sum;
+        final int year = 2025; // final -> const
+        sum = a + b;
+
+        System.out.println("Hello and welcome!");
+        System.out.println(sum);
+    }
+}
