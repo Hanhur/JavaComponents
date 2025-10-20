9e83fd794f83a44cb6361c7d5ae91fd30a969e07
Vladimír Hanhur <aqgz@email.cz>
2025-10-20 19:34:14 +0200

    update

---
+++ Lesson_15/src/A.java
@@ -0,0 +1,14 @@
+public class A {
+    int x = 5, y = 5;
+
+    public void sum()
+    {
+        System.out.println(x + y);
+    }
+
+    public void kv()
+    {
+        System.out.println("Из класс A");
+        System.out.println(x * x);
+    }
+}
---
+++ Lesson_15/src/B.java
@@ -0,0 +1,14 @@
+public class B extends A{
+    int z = 20;
+
+    public void multiply()
+    {
+        System.out.println(x * y * z);
+    }
+
+    public void kv()
+    {
+        System.out.println("Из класс B");
+        System.out.println(z * z);
+    }
+}
