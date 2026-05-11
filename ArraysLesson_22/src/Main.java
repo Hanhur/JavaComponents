72bcf46fb58736c69d067bc31c5e908283587d52
Vladimír Hanhur <aqgz@email.cz>
2026-05-11 12:21:39 +0200

    update

---
+++ ArraysLesson_22/src/Main.java
@@ -0,0 +1,135 @@
+import java.util.Scanner;
+import  java.util.Random;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 15;
+        int [] A = new int[n];
+
+        System.out.println("Заполнение массива на 15 элементов числами Фибоначчи");
+        A[0] = 0;
+        A[1] = 1;
+
+        for (int i = 2; i < n; i++)
+        {
+            A[i] = A[i - 2] + A[i - 1];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            System.out.printf("%d ", A[i]);
+        }
+
+// ====================================================================================================================
+
+        int[] B = new int[] {2,2,2,2,2,2,2,2,2,2};
+        int flag = 1;
+
+        for (int i = 0; i < B.length - 1; i++)
+        {
+            if (B[i] != B[i + 1])
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if(flag == 0) System.out.print("\nNo\n");
+        else System.out.print("\nYes\n");
+
+// ====================================================================================================================
+
+        int[] C = new int[] {1,2,4,12,6,5,8,56,21,277};
+        flag = 1;
+
+        for (int i = 0; i < C.length - 1; i++)
+        {
+            for (int j = i + 1; j < C.length; j++)
+            {
+                if (C[i] == C[j])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 0) break;
+        }
+        if(flag == 0) System.out.print("\nNo\n");
+        else System.out.print("\nYes\n");
+
+// ====================================================================================================================
+
+        int[] D = new int[] {1,2,4,12,1,5,8,56,2,12};
+        int count, check;
+
+        for (int i = 0; i < D.length; i++)
+        {
+            check = 0;
+            for (int k = i; k > 0; k--)
+            {
+                if (D[i] == D[k - 1]) check = 1;
+            }
+
+            if (check == 0)
+            {
+                count = 1;
+                for (int j = i + 1; j < D.length; j++)
+                {
+                    if (D[i] == D[j]) count++;
+                }
+                if(count > 1) System.out.printf("\nЧисло %d повторяется %d раз\n", D[i], count);
+            }
+        }
+
+// ====================================================================================================================
+
+        Random random = new Random();
+        int nn;
+
+        System.out.print("\nВведите размерность массива\n");
+        nn = scanner.nextInt();
+        int[] E = new int[nn];
+        int numMin = -10, numMax = nn;
+
+        System.out.print("\nВведите элементы массива\n");
+
+        for (int i = 0; i < nn; i++)
+        {
+            E[i] = random.nextInt((numMax - numMin) + 1) + numMin;
+        }
+
+        System.out.printf("\nИсходный массив\n");
+        for (int i = 0; i < nn; i++)
+        {
+            System.out.printf("%d ", E[i]);
+        }
+
+        flag = 0;
+        for (int i = 0; i < nn; i++)
+        {
+            if (E[i] < -1)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 1)
+        {
+            for (int i = 0; i < nn; i++)
+            {
+                if (E[i] < 0)
+                {
+                    E[i] = (int) Math.pow((double) E[i], (double) 2);
+                }
+            }
+        }
+
+        System.out.printf("\nРезультирующий массив\n");
+        for (int i = 0; i < nn; i++)
+        {
+            System.out.printf("%d ", E[i]);
+        }
+    }
+}
